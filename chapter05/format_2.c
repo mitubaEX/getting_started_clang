2aefedff71b9b28a1e6c6f1d11fde38cd4e9ce74
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-04-19 13:49:12 +0900

    finish chapter05

---
+++ chapter05/format_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int return_val;
+
+  return_val = printf("hello, world\n");
+  printf("return_val is %d\n", return_val);
+
+  printf("return_val is %d\n", printf("hello, world\n"));
+  return 0;
+}
---
+++ chapter05/input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  char one_string[16];
+
+  printf("input> ");
+
+  fgets(one_string, 16, stdin);
+
+  printf("Your input is %s", one_string);
+
+  return 0;
+}
