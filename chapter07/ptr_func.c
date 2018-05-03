e3fd51c627ee0518a0fe3766b77e43d747f4f7f0
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-05-03 23:58:15 +0900

    途中だけどcommitする

---
+++ chapter07/ptr_func.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int sum(int, int);
+
+int sum(int a, int b) {
+  int return_value;
+
+  return_value = a + b;
+
+  return return_value;
+}
+int main(int argc, char *argv[]) {
+  int num_1;
+  int num_2;
+  int answer;
+  int (*ptr_function) (int, int);
+
+  num_1 = 1;
+  num_2 = 2;
+
+  ptr_function = sum;
+
+  answer = ptr_function(num_1, num_2);
+
+  printf("answer = %d\n", answer);
+
+  return 0;
+}
